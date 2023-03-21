5673ab97cbaa50664a9d00d87202e02bed1287ff
Marco Inchingoli <evilmarkus@hotmail.it>
2023-03-21 13:07:55 +0100

    Add RouterView to App.vue, add router-link to nav

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ export default {
 
 <template>
     <AppNav />
+    <RouterView />
     <AppFooter />
 </template>
 
--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -8,7 +8,7 @@ export default {
 <template>
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Games Manager Project</a>
+            <router-link class="navbar-brand" :to="{ name: 'my-lists' }">Games Manager Project</router-link>
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
                 data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -17,11 +17,11 @@ export default {
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#" aria-current="page">Le mie liste<span
-                                class="visually-hidden">(current)</span></a>
+                        <router-link class="nav-link active" :to="{ name: 'my-lists' }" aria-current="page">Le mie
+                            liste<span class="visually-hidden">(current)</span></router-link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Scopri</a>
+                        <router-link class="nav-link" :to="{ name: 'discovery' }">Scopri</router-link>
                     </li>
                 </ul>
             </div>

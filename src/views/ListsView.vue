f0b133cf44793b374e0f4e14c4a481fbded0f15a
Marco Inchingoli <evilmarkus@hotmail.it>
2023-03-22 00:28:38 +0100

    Add tetris background, fix title lists

--- src/views/ListsView.vue
+++ src/views/ListsView.vue
@@ -8,21 +8,24 @@ export default {
     <div class="container-fluid">
 
         <section class="card_lists">
-            <div class="container">
+            <div class="container color_dark">
                 <!-- card list titoli da giocare -->
-                <div class="want_play_list mt-2">
-                    <h5 class="fs-4">Da giocare</h5>
-                    <div class="card border-0 shadow-lg"></div>
+                <div class="want_play_list mt-4">
+                    <div class="card border-0 shadow-lg">
+                        <h5 class="fs-3 text-center">Da giocare</h5>
+                    </div>
                 </div>
                 <!-- card list titoli in corso -->
-                <div class="playing_list mt-2">
-                    <h5 class="fs-4">Sto giocando a</h5>
-                    <div class="card border-0 shadow-lg"></div>
+                <div class="playing_list mt-4">
+                    <div class="card border-0 shadow-lg">
+                        <h5 class="fs-3 text-center">Sto giocando a</h5>
+                    </div>
                 </div>
                 <!-- card list titoli completati -->
-                <div class="played_list mt-2 mb-3">
-                    <h5 class="fs-4">Completati</h5>
-                    <div class="card border-0 shadow-lg"></div>
+                <div class="played_list mt-4 mb-4">
+                    <div class="card border-0 shadow-lg">
+                        <h5 class="fs-3 text-center">Completati</h5>
+                    </div>
                 </div>
             </div>
 
@@ -38,12 +41,19 @@ export default {
     background-color: $gm-secondary;
     padding-top: 10px;
     padding-bottom: 10px;
+    background-image: url('../../public/img/tetris-bg.png');
+    background-size: auto 50%;
+    background-position: center;
+    background-repeat: repeat-y;
 }
-
 
 .card {
     height: 450px;
-    background-color: $gm-primary-soft;
+    background-color: $gm-primary;
 
+}
+
+.color_dark {
+    color: $gm-dark;
 }
 </style>
